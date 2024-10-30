5635d7b3ac0aea688abfbfc021ebf980de45351a
Srinivas Lakshmanan <seenu73srinivas@gmail.com>
2024-10-30 12:01:16 +0530

    javadatatypes

---
+++ datatype.java
@@ -0,0 +1,12 @@
+
+class datatype
+{
+public static void main(String a[]){
+
+float marks= 6.5f;
+ byte by=124;
+ boolean b=true;
+ long lo= 5854l;
+System.out.println(marks + lo+by);
+}
+}
--- hello.java
+++ hello.java
@@ -3,9 +3,6 @@ class hello
 {
 public static void main(String a[])
 {
- float marks= 6.5
- byte 
- System.out.println(result);
-
+System.out.println("hello World");
 }
 }
